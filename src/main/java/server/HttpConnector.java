364eb43b860d01876373a3611e11dbccbf4f0e42
ichigo-xin <1219869429@qq.com>
2024-08-03 09:56:07 +0800

    第5课

--- src/main/java/server/HttpConnector.java
+++ src/main/java/server/HttpConnector.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class HttpConnector implements Runnable{
-
-
-    @Override
+public class HttpConnector implements Runnable {
+    int minProcessors = 3;
+    int maxProcessors = 10;
+    int curProcessors = 0;
+    //存放Processor的池子
+    Deque<HttpProcessor> processors = new ArrayDeque<>();
     public void run() {
-
         ServerSocket serverSocket = null;
         int port = 8080;
         try {
@@ -19,22 +22,58 @@ public class HttpConnector implements Runnable{
             e.printStackTrace();
             System.exit(1);
         }
+        // 初始化池子initialize processors pool
+        for (int i = 0; i < minProcessors; i++) {
+            HttpProcessor initprocessor = new HttpProcessor(this);
+            initprocessor.start();
+            processors.push(initprocessor);
+        }
+        curProcessors = minProcessors;
         while (true) {
             Socket socket = null;
             try {
                 socket = serverSocket.accept();
-                HttpProcessor httpProcessor = new HttpProcessor();
-                httpProcessor.process(socket);
-                socket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                //对每一个socket，从池子中拿到一个processor
+                HttpProcessor processor = createProcessor();
+                if (processor == null) {
+                    socket.close();
+                    continue;
+                }
+                //分配给这个processor
+                processor.assign(socket);
+                // Close the socket
+//                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-
     }
-
     public void start() {
         Thread thread = new Thread(this);
         thread.start();
+    }
+    //从池子中获取一个processor，池子为空且数量小于最大限制则会新建一个processor
+    private HttpProcessor createProcessor() {
+        synchronized (processors) {
+            if (processors.size() > 0) {
+                return ((HttpProcessor) processors.pop());
+            }
+            if (curProcessors < maxProcessors) {
+                return (newProcessor());
+            }
+            else {
+                return (null);
+            }
+        }
+    }
+    private HttpProcessor newProcessor() {
+        HttpProcessor initprocessor = new HttpProcessor(this);
+        initprocessor.start();
+        processors.push(initprocessor);
+        curProcessors++;
+        return ((HttpProcessor) processors.pop());
+    }
+    void recycle(HttpProcessor processor) {
+        processors.push(processor);
     }
 }
--- src/main/java/server/HttpProcessor.java
+++ src/main/java/server/HttpProcessor.java
@@ -6,21 +6,53 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 
-public class HttpProcessor {
+public class HttpProcessor implements Runnable {
+    Socket socket;
+    boolean available = false;
+    HttpConnector connector;
 
-    public HttpProcessor() {
+    public HttpProcessor(HttpConnector connector) {
+        this.connector = connector;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 等待分配下一个 socket
+            Socket socket = await();
+            if (socket == null) continue;
+            // 处理来自这个socket的请求
+            process(socket);
+            // 完成此请求
+            connector.recycle(this);
+        }
+    }
+
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
     }
 
     public void process(Socket socket) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
         InputStream input = null;
         OutputStream output = null;
         try {
             input = socket.getInputStream();
             output = socket.getOutputStream();
+            // 创建请求对象并解析
             Request request = new Request(input);
             request.parse();
+            // 创建响应对象
             Response response = new Response(output);
             response.setRequest(request);
+//               response.sendStaticResource();
+            // 检查这是对servlet还是静态资源的请求
+            // a request for a servlet begins with "/servlet/"
             if (request.getUri().startsWith("/servlet/")) {
                 ServletProcessor processor = new ServletProcessor();
                 processor.process(request, response);
@@ -28,12 +60,39 @@ public class HttpProcessor {
                 StaticResourceProcessor processor = new StaticResourceProcessor();
                 processor.process(request, response);
             }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            // 关闭 socket
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
+    synchronized void assign(Socket socket) {
+        // 等待 connector 提供新的 Socket
+        while (available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        // 存储新可用的 Socket 并通知我们的线程
+        this.socket = socket;
+        available = true;
+        notifyAll();
+    }
 
-
+    private synchronized Socket await() {
+        // 等待 connector 提供一个新的 Socket
+        while (!available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        // 通知Connector我们已经收到这个Socket了
+        Socket socket = this.socket;
+        available = false;
+        notifyAll();
+        return (socket);
     }
 }
